10ad1e626711c45b8bb4f75cefa10dd0355f4827
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-04-02 23:04:05 +0100

    edited app.js file

--- app.js
+++ app.js
@@ -7,13 +7,6 @@ const cors = require('cors')
 
 const app = express();
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-    connectionMethod()
-})
-
 // middlewares
 app.use(cors());
 app.use(express.json())
@@ -25,4 +18,11 @@ app.use('/assessment', require('./routes/student/studentRoutes'))
 
 app.get('/', (req, res) => {
     res.send("Home Route")
+})
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+    connectionMethod()
 })
